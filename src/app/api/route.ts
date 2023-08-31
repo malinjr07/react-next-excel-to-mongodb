e7e540e7650a150d993d4abbc3e4a81200e602d7
malinjr07 <malinjr07@gmail.com>
2023-08-31 19:49:05 +0600

    package ongoing

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,15 +3,29 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
   const res = await request.json();
-  const { urlString } = res;
-  const urlArr = urlString.split('/');
-  if (urlArr.length === 4) {
-    const client = await MongoClient.connect(urlString);
-    const db = client.db(urlArr.at(-1));
-    return NextResponse.json({ client, db });
+  const { urlString, collection } = res;
+  const urlArr: string[] = urlString.split('/');
+  const urlOnlyArr = [...urlArr];
+  urlOnlyArr.pop();
+  const urlOnly = urlOnlyArr.join('/');
+  if (urlArr.length === 4 && collection) {
+    try {
+      const client = await MongoClient.connect(urlOnly);
+      const db = client.db(urlArr.at(-1));
+      return NextResponse.json(
+        { success: 'Yay! MongoDB connected!' },
+        { status: 200 }
+      );
+    } catch (error) {
+      console.error('error found', error);
+      return NextResponse.json(
+        { error: 'error found!:' + error },
+        { status: 500 }
+      );
+    }
   } else {
     return NextResponse.json(
-      { error: 'No Valid Mongo DB URL Found' },
+      { error: 'No Valid MongoDB URL or Collection Name Found' },
       { status: 500 }
     );
   }
--- src/components/MongoDbUrl.tsx
+++ src/components/MongoDbUrl.tsx
@@ -4,24 +4,93 @@ import React, { ChangeEvent, FormEvent, useState } from 'react';
 
 const MongoDbUrl = () => {
   const [urlString, setUrlString] = useState('');
-  async function validateUrl(event: FormEvent<HTMLFormElement>) {
+  const [collection, setCollection] = useState('');
+  function validateUrl(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    const urlArr = urlString.split('/');
-    if (urlArr.length === 4) {
+    if (urlString && collection) {
+      /*
+      Upload Sheet API
+      fetch('/api', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          urlString,
+          collection,
+        }),
+      })
+        .then(async (response) => {
+          const value = await response.json();
+          console.log(' MongoDbUrl.tsx:22 ~ response:', value);
+        })
+        .catch((error) => {
+          console.error('Error', error);
+        }); */
+    } else if (!urlString) {
+      console.error('No MongoDB URL was Found!');
+    } else if (!collection) {
+      console.error('No Collection Name was Found!');
     } else {
-      console.error('No Valid Mongo DB URL Found');
+      return false;
     }
   }
   return (
     <form onSubmit={validateUrl}>
       <div className='flex flex-col gap-3 justify-start items-start '>
-        <div className='w-full flex justify-between items-center  '>
-          <label
-            htmlFor='mongoUrl'
-            className='text-lg leading-loose tracking-wide  '
-          >
-            MongoDB URL
-          </label>{' '}
+        <div className='flex w-full flex-col justify-start items-start'>
+          <div className='w-full flex justify-between items-center  '>
+            <label
+              htmlFor='mongoUrl'
+              className='text-lg leading-loose tracking-wide  '
+            >
+              MongoDB URL*
+            </label>{' '}
+          </div>
+
+          <textarea
+            name=''
+            id='mongoUrl'
+            onChange={(e: ChangeEvent<HTMLTextAreaElement>) => {
+              setUrlString(e.target.value);
+            }}
+            required
+            className='px-6 py-4 rounded-xl border border-[#E6E6E6] border-r-transparent w-full resize-none '
+            cols={30}
+            rows={10}
+          />
+
+          <p className='text-xs mt-2 text-[#737373]'>
+            Please Add your MongoDB URL. <br /> This URL won&apos;t get stored
+            anywhere. Upon Reload, your URL will get removed.
+          </p>
+        </div>
+        <div className='flex w-full flex-col justify-start items-start'>
+          <div className='w-full flex justify-between items-center  '>
+            <label
+              htmlFor='mongoUrl'
+              className='text-lg leading-loose tracking-wide  '
+            >
+              Collection Name*
+            </label>{' '}
+          </div>
+
+          <input
+            type='text'
+            name=''
+            id='collection'
+            required
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              setCollection(e.target.value);
+            }}
+            className='px-6 py-4 rounded-xl border border-[#E6E6E6] border-r-transparent w-full resize-none '
+          />
+
+          <p className='text-xs mt-2 text-[#737373]'>
+            Please Type your MongoDB Collection Name.
+          </p>
+        </div>
+        <div className='w-full flex flex-row justify-end items-center'>
           <button
             type='submit'
             className='px-5 bg-[#23A6F0] rounded-xl flex justify-center items-center py-3 text-white text-lg '
@@ -29,22 +98,6 @@ const MongoDbUrl = () => {
             Validate
           </button>
         </div>
-
-        <textarea
-          name=''
-          id='mongoUrl'
-          onChange={(e: ChangeEvent<HTMLTextAreaElement>) => {
-            setUrlString(e.target.value);
-          }}
-          className='px-6 py-4 rounded-xl border border-[#E6E6E6] border-r-transparent w-full resize-none '
-          cols={30}
-          rows={10}
-        />
-
-        <p className='text-xs text-[#737373]'>
-          Please Upload your MongoDB URL. <br /> This URL won&apos;t get stored
-          anywhere. Upon Reload, your URL will get removed.
-        </p>
       </div>
     </form>
   );
